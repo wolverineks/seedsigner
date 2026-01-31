0c923b6b3dc7725a1bf3792eacfd52ad83778614
Kevin <wolverine.ks@gmail.com>
2026-01-31 15:31:52 -0600

    test display on device

--- src/seedsigner/hardware/hardware.py
+++ src/seedsigner/hardware/hardware.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
 
+screen_height = 240
+screen_width = 320
+
 
 @dataclass
 class Platform:
-    screen_height = 240
-    screen_width = 320
+    screen_height = screen_height
+    screen_width = screen_width
+    screen_dimensions = (screen_width, screen_height)
     # screen_width = 240
--- src/seedsigner/main.py
+++ src/seedsigner/main.py
@@ -1,17 +1,36 @@
+# from PIL import Image, ImageDraw
+# from time import sleep
+# import pygame
+
+# from hardware.hardware import Platform
+
+# pygame.init()
+# screen = pygame.display.set_mode(Platform.screen_dimensions)
+
+# image = Image.new("RGB", Platform.screen_dimensions, "red")
+# draw = ImageDraw.Draw(image)
+# draw.text((120, 120), "IT WORKS", fill="white", font_size=30, anchor="mm")
+
+# pg_img = pygame.image.fromstring(image.tobytes(), image.size, image.mode)
+# pygame.event.pump()  # Pump events to keep the window responsive even when no input is read
+# screen.blit(pg_img, (0, 0))
+# pygame.display.flip()
+
+# sleep(4)
+
+
 from PIL import Image, ImageDraw
-from hardware.displays.display_driver import DisplayDriver
+from time import sleep
 
 from hardware.hardware import Platform
-
-driver = DisplayDriver(
-    "st7789",
-    width=int(Platform.screen_width),
-    height=int(Platform.screen_height),
-)
+from hardware.displays.ST7789 import ST7789
 
 
-img = Image.new("RGB", (Platform.screen_width, Platform.screen_height), "black")
-draw = ImageDraw.Draw(img)
-draw.text((120, 120), "IT WORKS", fill="white", anchor="mm")
+image = Image.new("RGB", Platform.screen_dimensions, "red")
+draw = ImageDraw.Draw(image)
+draw.text((120, 120), "IT WORKS", fill="white", font_size=30, anchor="mm")
 
-driver.show_image(img)
+driver = ST7789()
+driver.show_image(image, 0, 0)
+
+sleep(4)
