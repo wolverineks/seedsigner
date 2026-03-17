feaaefda540012010eaf104ad9b48f139126da29
Kevin <wolverine.ks@gmail.com>
2026-03-16 20:39:57 -0500

    build test

--- src/main.py
+++ src/main.py
@@ -2,23 +2,22 @@ from time import sleep
 
 from PIL import Image, ImageDraw  # type: ignore
 
-from seedsigner.dimensions import Dimensions
 from seedsigner.hardware.displays.display_driver import DisplayDriver
 
 
 def main() -> None:
-    # driver = DisplayDriver("desktop", Dimensions.width, Dimensions.height)
-    driver = DisplayDriver("st7789", Dimensions.width, Dimensions.height)
+    # driver = DisplayDriver("desktop", 240, 240)
+    driver = DisplayDriver("st7789", 240, 240)
 
-    canvas = Image.new("RGB", (Dimensions.width, Dimensions.height), "black")
+    canvas = Image.new("RGB", (240, 240), "black")
     draw = ImageDraw.Draw(canvas)
-    draw.rectangle((0, 0, Dimensions.width - 1, Dimensions.height - 1), outline="white")
+    draw.rectangle((0, 0, 240 - 1, 240 - 1), outline="white")
     draw.rectangle(
-        (20, 20, Dimensions.width - 21, Dimensions.height - 21), outline="white"
+        (20, 20, 240 - 21, 240 - 21), outline="white"
     )
-    draw.line((0, 0, Dimensions.width - 1, Dimensions.height - 1), fill="red", width=2)
+    draw.line((0, 0, 240 - 1, 240 - 1), fill="red", width=2)
     draw.line(
-        (Dimensions.width - 1, 0, 0, Dimensions.height - 1), fill="green", width=2
+        (240 - 1, 0, 0, 240 - 1), fill="green", width=2
     )
 
     while True:
